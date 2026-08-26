072a7da585321685a469595ebfd89d8eab37fac4
devHerz <larissa.sandrine.ai@gmail.com>
2026-08-26 15:45:39 +0000

    Fix: Correction des autorisations Android et receveurs pour éviter le crash a l installation sur mobile

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,19 +22,19 @@ android {
 
     defaultConfig {
         applicationId = "com.devherz.my_salary"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
+        multiDexEnabled = true
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debug keys for now, so release build works seamlessly
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
